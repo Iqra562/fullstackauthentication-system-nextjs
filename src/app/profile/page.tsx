83b7e88c205bb87a09723bd34fddf0b18123e17f
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2026-01-08 02:18:27 +0500

    logout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,9 +3,18 @@ import Link from "next/link";
 import React, { use } from "react";
 import { useRouter } from "next/navigation";
 import { FaArrowLeftLong } from "react-icons/fa6";
+import axios from "axios";
 
 export default function ProfilePage() {
-   
+   const router = useRouter();
+   const logout = async () =>{
+    try{
+      await axios.get('/api/users/logout');
+      router.push("/login")
+    }catch(error:any){ 
+   console.log(error.messsage)
+    }
+   }
     return (
               <div className="h-screen">
 <div className=" py-2 px-2 ">
@@ -23,6 +32,7 @@ export default function ProfilePage() {
                 PROFILE PAGE
               </div>
             </div>
+            <button onClick={logout} className="bg-green-800 px-2 py-2 rounded-md ">Logout</button>
         </div>
         </div>
     )
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -29,5 +29,5 @@ verifyToken:String,
 verifyTokenExpiry:Date
 })
 
-const User = mongoose.models.users || mongoose.model("Users",userSchema);
+const User = mongoose.models.Users || mongoose.model("Users",userSchema);
 export default User;
